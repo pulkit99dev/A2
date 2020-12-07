ff899d768a304cee17591037923dc7310f535889
pulkit99dev <pulkitnagpal987654321@gmail.com>
2020-12-07 22:59:24 +0530

    4th

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,6 @@
 module.exports.home = function(req, res){
-    return res.end('Accessing Controller')
-}
+    //return res.end('Accessing Controller')
+    return res.render('home', {
+        title : 'Application Home Page'
+    });
+};
